2eb255688e68e917fc3eca2ba56713e0aa8365e1
OmarDer <odervisevi1@etf.unsa.ba>
2016-06-05 15:13:47 +0200

    OpenShift DBVariables

--- Spirala4/notifications.php
+++ Spirala4/notifications.php
@@ -11,8 +11,8 @@ function rest_get($request, $data) {
     define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME')); 
     define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD')); 
     define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME')); 
-    $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT."';"; 
-    $veza = new PDO($dsn, DB_USER);
+    $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT; 
+    $veza = new PDO($dsn, DB_USER, DB_PASS);
 //$veza = new PDO("mysql:dbname=wnclub;host=127.4.179.2;port=3306;charset=utf8","DBuser");
 $veza->exec("set names utf8");
 $autor=$_GET['autor'];
